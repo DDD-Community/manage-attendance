d4f9b097f5ab829be2a44e59b09bc8ca5f2165a8
ufxpri <cfi02222@gmail.com>
2024-10-20 16:29:37 +0000

    first commit

---
+++ ddd_app_server/ddd_app_server/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import include, path
+# from rest_framework.routers import DefaultRouter
+
+# # Assuming you are using DRF viewsets
+# router = DefaultRouter()
+
+# URL patterns
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # path("", include(router.urls)),  # Default router, if using viewsets
+    path("api-auth/", include('rest_framework.urls', namespace='rest_framework')),
+    path("invite-code/", include('invite_code.urls')),  # Include invite code URLs
+    path("members/", include('members.urls')),  # Include member-related URLs
+    path("schedules/", include('schedules.urls')),  # Include schedule-related URLs
+]
